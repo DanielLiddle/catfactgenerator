faa28164ae82182d45de7000b6d7ec34f947374d
DanielLiddle <48800605+DanielLiddle@users.noreply.github.com>
2023-02-06 20:26:57 +0000

    Update code.py

--- code.py
+++ code.py
@@ -1,18 +1,8 @@
-!pip install openai
-import openai
+import streamlit as st
 
-# Use your OpenAI API key here
-openai.api_key = "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"
+st.title("SEO Assistant")
 
-def generate_cat_fact():
-  response = openai.Completion.create(
-    engine="text-davinci-002",
-    prompt="Tell me a cat fact",
-    max_tokens=100,
-    n=1,
-    stop=None,
-    temperature=0.5,
-  )
+user_query = st.text_input("What is your question?")
 
-  message = response["choices"][0]["text"].strip()
-  return message
+if user_query:
+    st.write("It depends.")
